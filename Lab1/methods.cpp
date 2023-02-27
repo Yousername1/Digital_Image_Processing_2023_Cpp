1fe2b1868b475be34174bd19aaa3dc8414aa2fff
Ram 00 <ram00play@gmail.com>
2023-02-26 23:22:23 -0900

    error calculation

--- Lab1/methods.cpp
+++ Lab1/methods.cpp
@@ -41,10 +41,31 @@ Mat getHistColor(const Mat& image)
 	return Mat();
 }
 
+void errorCalculation(int Y, int Y_quanti, int rowS, int colS, int num_level)
+{
+	double sum = 0;
+	for (int r = 0; r <= rowS - 1; r++) {
+		for (int c = 0; c <= colS - 1; c++) {
+			sum += pow((Y - Y_quanti), 2);
+		}
+	}
+	double error = sqrt((1 / rowS * colS) * sum);
+	double error_mid = num_level / sqrt(12);
+
+	string result = to_string(error) + ", " + to_string(error_mid);
+
+	map<int, string> errorsTable;
+	errorsTable[num_level] = result;
+	map<int, string> ::iterator it = errorsTable.begin();
+	for (int i = 0; it != errorsTable.end(); it++) {
+		cout << i << " level - " << it->first << " srkv, mid" << it->second << endl;
+	}
+}
+
 Mat doQuantization(const Mat& image, int q_level)
 {
 	Mat resultImage = Mat::zeros(image.rows, image.cols, CV_8UC1);
-	int Y = 0;
+	int Y = 0, Y_quanti = 0;
 
 	//q_level - number of quantization levels (2, 4 ... 64)
 	int range = 255 / (q_level - 1);
@@ -56,17 +77,19 @@ Mat doQuantization(const Mat& image, int q_level)
 
 			for (int level = 0; level <= range; level++) {
 				if (Y > range * level && Y <= range * level + range / 2) {
-					Y = range * level;
+					Y_quanti = range * level;
 				}
 				else if (Y > range * level + range / 2 && Y <= range * (level + 1)) {
-					Y = range * (level + 1);
+					Y_quanti = range * (level + 1);
 				}
 			}
 
-			resultImage.at<uchar>(i, j) = Y;
+			resultImage.at<uchar>(i, j) = Y_quanti;
 		}
 	}
 
 	imshow("Hist for quanti", getHist(resultImage));
+	errorCalculation(Y, Y_quanti, resultImage.rows, resultImage.cols, q_level);
+
 	return resultImage;
 }
--- Lab1/methods.h
+++ Lab1/methods.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+#include <map>
 
 #pragma once
 
@@ -10,6 +12,7 @@ using namespace std;
 Mat getHist(const Mat &image);
 Mat getHistColor(const Mat& image);
 Mat doQuantization(const Mat& image, int q_level);
+double errorCalculation(int Y, int Y_quanti, int rowS, int colS);
 
 
 
